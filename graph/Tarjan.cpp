1e12519ac516b749245119f1a0ca1d0b14b65ae0
gantheory <b03901133@ntu.edu.tw>
2017-09-27 09:55:32 +0800

    add Tarjans algorithm

---
+++ graph/Tarjan.cpp
@@ -0,0 +1,20 @@
+// Build: O( V^2 ), Query: O( 1 )
+// n: the number of nodes
+int graph[ N ][ N ], lca[ N ][ N ];
+vector< bool > visit( N, false );
+
+void tarjan( int now ) {
+  if ( visit[ now ] )
+    return;
+  visit[ now ] = true;
+
+  for ( int i = 1 ; i <= n ; ++i )
+    if ( visit[ i ] )
+      lca[ now ][ i ] = lca[ i ][ now ] = st.Find( i );
+
+  for ( int i = 1 ; i <= n ; ++i )
+    if ( g[ now ][ i ] < 1e9 and !visit[ i ] ) {
+      tarjan( i );
+      st.Union( i, now );
+    }
+}
