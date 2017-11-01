720f0aaeb40215f5bfbe621e77e46f4a16010524
Daikon-Sun <sunfankeng@gmail.com>
2017-11-01 20:35:16 +0800

    upgrade BipartiteMatching

--- flow/BipartiteMatching.cpp
+++ flow/BipartiteMatching.cpp
@@ -1,69 +1,72 @@
-// O( ( V + E ) * sqrt( V ) )
-class BipartiteMatching {
- public:
-  static const int N = 1e5 + 10;  // total number of nodes, n + m
-  static const int NIL = 0;
-  static const int INF = ( 1 << 28 );
-  vector< int > G[ N ];
+struct BipartiteMatching { // O( ( V + E ) * sqrt( V ) )
+  vector< int > G[ N ]; // N = total number of nodes = n + m
   int n, m, match[ N ], dist[ N ];
   // n: number of nodes on left side, nodes are numbered 1 to n
   // m: number of nodes on right side, nodes are numbered n+1 to n+m
   // G = NIL[0] ∪ G1[G[1---n]] ∪ G2[G[n+1---n+m]]
   bool BFS() {
-    int i, u, v, len;
     queue< int > Q;
-    for ( i = 1; i <= n; i++ ) {
-      if ( match[ i ] == NIL ) {
+    for (int i = 1; i <= n; i++ ) {
+      if ( match[ i ] == 0 ) {
         dist[ i ] = 0;
         Q.push( i );
       }
-      else
-        dist[ i ] = INF;
+      else dist[ i ] = INF;
     }
-    dist[ NIL ] = INF;
+    dist[ 0 ] = INF;
     while ( !Q.empty() ) {
-      u = Q.front();
-      Q.pop();
-      if ( u != NIL ) {
-        len = G[ u ].size();
-        for ( i = 0; i < len; i++ ) {
-          v = G[ u ][ i ];
+      int u = Q.front(); Q.pop();
+      if ( dist[ u ] < dist[ 0 ] )
+        for(int v : G[u])
           if ( dist[ match[ v ] ] == INF ) {
             dist[ match[ v ] ] = dist[ u ] + 1;
             Q.push( match[ v ] );
           }
-        }
-      }
     }
-    return ( dist[ NIL ] != INF );
+    return ( dist[ 0 ] != INF );
   }
   bool DFS( int u ) {
-    int i, v, len;
-    if ( u != NIL ) {
-      len = G[ u ].size();
-      for ( i = 0; i < len; i++ ) {
-        v = G[ u ][ i ];
-        if ( dist[ match[ v ] ] == dist[ u ] + 1 ) {
-          if ( DFS( match[ v ] ) ) {
-            match[ v ] = u;
-            match[ u ] = v;
-            return true;
-          }
+    if ( u != 0 ) {
+      for(int v : G[u])
+        if ( dist[ match[ v ] ] == dist[ u ] + 1 and DFS( match[ v ] ) ) {
+          match[ v ] = u;
+          match[ u ] = v;
+          return true;
         }
-      }
       dist[ u ] = INF;
       return false;
     }
     return true;
   }
-  int HopcroftKarp() {
-    int matching = 0, i;
-    // match[] is assumed NIL for all vertex in G
+  int Max_Match() {
+    int matching = 0;
+    fill_n(match, n+m+1, 0);
     while ( BFS() )
-      for ( i = 1; i <= n; i++ )
-        if ( match[ i ] == NIL && DFS( i ) ) matching++;
+      for (int i = 1; i <= n; i++ )
+        if ( match[ i ] == 0 and DFS( i ) ) matching++;
     return matching;
   }
   void AddEdge( int u, int v ) { G[ u ].push_back( n + v ); }
-  int Solve() { return HopcroftKarp(); }
-};
+  void DFS2(int u) {
+    dist[u] = 1;
+    for(int v : G[u])
+      if(v != match[u]) {
+        dist[v] = 1;
+        if(match[v] != 0)
+          DFS2(match[v]);
+      }
+  }
+  void Min_Vertex_Cover(vector<int> &lrtn, vector<int> &rrtn) {
+    //after calling Max_Match
+    fill_n( dist + 1, n + m, 0);
+    for( int i = 1; i <= n; i++ )
+      if( match[i] == 0 )
+        DFS2( i );
+    for( int i = 1; i <= n; i++ )
+      if( dist[i] == 0 )
+        lrtn.push_back( i );
+    for(int i = n + 1; i <= n + m; i++ )
+      if( dist[i] == 1 )
+        rrtn.push_back( i - n );
+  
+} ob;
